a5bcc3e98773f954cbef3d3e5c52e64160e70e6b
Rajan Joshi <rajanjoshi@Rajans-MacBook-Pro.local>
2025-06-04 01:41:43 +0200

    add mlops-course-2

---
+++ mlops-course-2/scripts/tf-backend-resources.sh
@@ -0,0 +1,27 @@
+AWS CLI
+-------
+
+# Specify the username for the new IAM user
+USER_NAME="terraform_user"
+
+# Create IAM User and capture the response
+USER_RESPONSE=$(aws iam create-user --user-name "$USER_NAME")
+
+# Check the Amazon Resource Name (ARN) from create-user response.
+echo $USER_RESPONSE                                          
+
+# Attach Admin Access Policy to IAM User
+aws iam attach-user-policy --user-name "$USER_NAME" --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+
+# Create Access and Secret Access Keys
+CREDS_JSON=$(aws iam create-access-key --user-name "$USER_NAME")
+
+# Check Access and Secret Access Keys from create-access-key response.
+echo $CREDS_JSON                                          
+
+# Create S3 Bucket
+S3_BUCKET_NAME="tf-remote-backend-ehb9129"
+aws s3 mb "s3://$S3_BUCKET_NAME" --region "eu-west-1"
+
+# Enable Versioning for S3 Bucket
+aws s3api put-bucket-versioning --bucket "$S3_BUCKET_NAME" --versioning-configuration Status=Enabled
---
+++ mlops-course-2/terraform/environments/dev.tfvars
@@ -0,0 +1,10 @@
+environment = "dev"
+location    = "eu-west-1"
+
+
+s3 = [
+  {
+    key  = "mlops-course-ehb-datastore"
+    tags = {}
+  }
+]
